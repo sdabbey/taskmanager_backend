28d7af09e7f9ad1b0d3504ebf9e1df6be1d09bc5
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-09-01 22:39:54 +0000

    update

--- taskmanager_backend/settings.py
+++ taskmanager_backend/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'PORT': '6719',
     }
 }
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
