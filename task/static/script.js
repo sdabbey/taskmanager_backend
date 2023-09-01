455f45b5d99e01a35d945ac684d4afc72fbf9569
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-09-01 22:10:29 +0000

    update

--- task/static/script.js
+++ task/static/script.js
@@ -47,6 +47,41 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
+
+  const delete_btns = document.querySelectorAll(".bx-trash")
+
+delete_btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        alert("hello")
+        fetch('/delete_task/'+ btn.id + '/', {
+        method: 'POST',
+        headers: {
+          'X-CSRFToken': csrfToken,
+        },
+        
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'success') {
+          const task_details = document.querySelectorAll('.task-detail');
+          task_details.forEach(task_detail => {
+            var itemId = task_detail.getAttribute('data-task-id');
+            if (itemId == btn.id){
+              task_detail.innerHTML = ''
+            }
+          })
+          
+          const sidetasks = document.querySelectorAll('.msg.anim-y');
+          sidetasks.forEach(task => {
+            var itemId = task.getAttribute('data-task-id');
+            if (itemId == btn.id){
+              task.remove()
+            }
+          })
+        }
+    })
+})
+})
   function getCurrentDate() {
     const now = new Date();
     const options = { year: 'numeric', month: 'short', day: 'numeric' };
